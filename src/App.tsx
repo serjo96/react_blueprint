92f6f2ac111f74c598ca338e24832e6f668905b7
serjo96 <promlemboy@gmail.com>
2024-01-28 18:21:07 +0700

    Added css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css';
+import styles from './App.module.css';
 import { AuthProvider } from '~/features/auth/cotext/auth-context';
 import AppRoutes from '~/routes/App-Routes';
 
 const App = () => {
   return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
+    <div className={styles.container}>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </div>
   );
 };
 
