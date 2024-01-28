13dbc0ed738ffb43f461e8ce5cbe86b819c9a392
serjo96 <promlemboy@gmail.com>
2024-01-29 00:15:29 +0700

    Updated home page;

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import { Container, Typography, Box, Link, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import ComputerIcon from '@mui/icons-material/Computer'; // Example icon
+import SecurityIcon from '@mui/icons-material/Security'; // Example icon
+import BuildIcon from '@mui/icons-material/Build'; // Example icon
 
 const HomePage = () => {
-  // Здесь может быть логика или состояние, специфичное для этой страницы
-
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Some home page</p>
-      {/* Здесь может быть дополнительный контент или функционал */}
-    </div>
+    <Container maxWidth="md">
+      <Box my={4}>
+        <Typography variant="h4" gutterBottom>
+          🚀 React Template Project
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Jumpstart your React applications with this ready-to-use template, featuring:
+        </Typography>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <ComputerIcon />
+            </ListItemIcon>
+            <ListItemText primary="Basic to advanced state management for scalability." />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <SecurityIcon />
+            </ListItemIcon>
+            <ListItemText primary="User authentication and registration capabilities." />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <BuildIcon />
+            </ListItemIcon>
+            <ListItemText primary="Efficient groundwork for both simple and complex web apps." />
+          </ListItem>
+        </List>
+        <Typography variant="body1" paragraph>
+          Extend your project with a full-stack solution using a complementary backend template:
+        </Typography>
+        <Typography variant="body1">
+          🔗 Backend:
+          <Link href="https://github.com/serjo96/nestjs-backend_blueprint" target="_blank" rel="noopener">
+            Nest.js + TypeORM + PostgreSQL
+          </Link>
+        </Typography>
+        <Typography variant="body1" paragraph>
+          This synergy ensures a seamless development journey from start to finish. 🌟
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
