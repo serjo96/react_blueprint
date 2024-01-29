76ece6f183fe5057cf6baac243c631c7e9792903
serjo96 <promlemboy@gmail.com>
2024-01-29 15:26:55 +0700

    Updated home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Container, Typography, Box, Link, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import ComputerIcon from '@mui/icons-material/Computer'; // Example icon
-import SecurityIcon from '@mui/icons-material/Security'; // Example icon
-import BuildIcon from '@mui/icons-material/Build'; // Example icon
+import ComputerIcon from '@mui/icons-material/Computer';
+import SecurityIcon from '@mui/icons-material/Security';
+import BuildIcon from '@mui/icons-material/Build';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+
 
 const HomePage = () => {
   return (
@@ -13,6 +16,9 @@ const HomePage = () => {
         </Typography>
         <Typography variant="body1" paragraph>
           This project serves as a template for quickly bootstrapping React applications that require basic state management with the potential for scaling up to more comprehensive state management solutions in the future. It includes essential features such as user authentication and registration, laying the groundwork for building both simple and complex web applications efficiently.
+        </Typography>
+        <Typography variant="body1" paragraph>
+          Authorized users can access the <Link href="/calendar">calendar page</Link>, while administrators have exclusive access to the <Link href="/admin-panel">admin panel</Link>.
         </Typography>
         <Typography variant="body1" paragraph>
           Jumpstart your React applications with this ready-to-use template, featuring:
@@ -35,6 +41,18 @@ const HomePage = () => {
               <BuildIcon />
             </ListItemIcon>
             <ListItemText primary="Efficient groundwork for both simple and complex web apps." />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <VerifiedUserIcon />
+            </ListItemIcon>
+            <ListItemText primary="Guards for route authorization, ensuring secure access to features based on user roles." />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <AdminPanelSettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Admin rights for enhanced control and management within the application." />
           </ListItem>
         </List>
         <Typography variant="body1" paragraph>
