f41ca1d03dfd610f053e975178c273941a2d2864
serjo96 <promlemboy@gmail.com>
2024-02-04 15:55:50 +0700

    Added global styles and reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from "react-router-dom";
 
+import './styles/reset.css';
+import './styles/global.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './index.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CustomThemeProvider from "~/services/themes/ThemeContext";
-import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
