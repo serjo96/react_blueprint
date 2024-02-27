90bf33760409d1f7b90e6f5c291e26ef259dfc7b
serjo96 <promlemboy@gmail.com>
2024-02-28 00:42:26 +0700

    Added open api generated api

---
+++ src/services/api/open-api/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AppControllerSendTestEmailRequest';
+export * from './CreateUserDto';
+export * from './EditUserDto';
+export * from './EmailVerificationEntity';
+export * from './ForgottenPasswordEntity';
+export * from './Profile';
+export * from './RefreshToken';
+export * from './UserEntity';
