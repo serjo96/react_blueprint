13fd62d93625344f90a2ab6c7d69e825f513e9cf
serjo96 <promlemboy@gmail.com>
2024-02-28 16:46:29 +0700

    Cleanup code;

--- src/services/api/middleware/refresh-token.middleware.ts
+++ src/services/api/middleware/refresh-token.middleware.ts
@@ -1,6 +1,6 @@
-import {DependencyInjector} from "~/core/dependencyInjector";
 import {RequestContext, ResponseContext} from "~/services/api/open-api";
 import {Tokens} from "~/features/auth/cotext/auth-context";
+import {DependencyInjector} from "~/utils/dependencyInjector";
 
 export const refreshTokenMiddleware = {
   pre: async (context: RequestContext) => {
@@ -8,10 +8,10 @@ export const refreshTokenMiddleware = {
   },
   post: async (context: ResponseContext) => {
     if (context.response.status === 403) {
-      // Логика для обновления токена
+      // Logic to update the token
       await DependencyInjector.refreshAccessToken();
       const token = localStorage.getItem(Tokens.ACCESS_TOKEN);
-      // Повторный запрос с обновленным токеном
+      // Re-request with updated token
       return context.fetch(context.url, {
         ...context.init,
         headers: {
