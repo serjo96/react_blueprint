8afbf08efb805761c79a3c060d48265aa8e403ce
serjo96 <promlemboy@gmail.com>
2024-02-29 23:06:15 +0700

    Translate error page

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,17 +1,19 @@
-// ErrorPage.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>Упс, произошла ошибка!</h1>
-      <p>Мы уже работаем над её устранением. Приносим извинения за неудобства.</p>
-      <p>Попробуйте:</p>
+    <div>
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+
+      <h1>Oops, there was an error!</h1>
+      <p>We are already working to eliminate it. We apologize for the inconvenience.</p>
+      </div>
+      <p>Try:</p>
       <ul>
-        <li>Перезагрузить страницу</li>
-        <li>Вернуться на <Link to="/">главную страницу</Link></li>
-        <li>Связаться с нами, если проблема повторится</li>
+        <li>Reload page</li>
+        <li>Return to <Link to="/">home page</Link></li>
+        <li>Contact us if the problem persists</li>
       </ul>
     </div>
   );
