c14f8feddb067b6e4540287f48fae37ecc75166b
serjo96 <promlemboy@gmail.com>
2024-02-29 23:05:45 +0700

    Added rememberMe validation

--- src/features/auth/validation/auth-validation.ts
+++ src/features/auth/validation/auth-validation.ts
@@ -3,6 +3,7 @@ import Joi from 'joi';
 export const loginValidationSchema = Joi.object({
   email: Joi.string().email({ tlds: { allow: false } }).required().label('Email'),
   password: Joi.string().min(6).required().label('Password'),
+  rememberMe: Joi.boolean(),
 });
 
 export const registrationValidationSchema = Joi.object({
