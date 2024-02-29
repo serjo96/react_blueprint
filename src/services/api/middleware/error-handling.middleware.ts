9f545c7719c522ea30232d418e25a7feb158852c
serjo96 <promlemboy@gmail.com>
2024-02-29 23:11:31 +0700

    Added handle error in middleware

--- src/services/api/middleware/error-handling.middleware.ts
+++ src/services/api/middleware/error-handling.middleware.ts
@@ -3,11 +3,13 @@ import {ErrorHandler} from "~/services/ErrorHandler";
 
 export const errorHandlingMiddleware = {
   post: async (context: ResponseContext) => {
+    let returnData = context.response;
     if (!context.response.ok) {
       // Handle API responses with errors
       const error = await context.response.json();
       ErrorHandler.handle(error);
+      returnData = error;
     }
-    return context.response;
+    return returnData;
   }
 };
