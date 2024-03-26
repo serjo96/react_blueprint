b7b2ab8683cc9198c7b6326cdbead385197a7d0a
serjo96 <promlemboy@gmail.com>
2024-03-27 01:20:47 +0700

    Added di methods for logut

--- src/utils/dependencyInjector.ts
+++ src/utils/dependencyInjector.ts
@@ -1,7 +1,16 @@
 export class DependencyInjector {
-  static refreshAccessToken: () => Promise<void>;
+  public static refreshAccessToken: () => Promise<string>;
+  public static logout: () => Promise<void>;
+  public static isRetryAttempt = false;
 
-  static injectRefreshTokenMethod(refreshTokenMethod: () => Promise<void>) {
+  public static injectRefreshTokenMethod(refreshTokenMethod: () => Promise<string>) {
     DependencyInjector.refreshAccessToken = refreshTokenMethod;
+  }
+
+  public static injectLogoutMethod(logout: ()=> Promise<void>) {
+    DependencyInjector.logout = logout
+  }
+  public static changeRetryAttempt(flag: boolean) {
+    this.isRetryAttempt = flag;
   }
 }
