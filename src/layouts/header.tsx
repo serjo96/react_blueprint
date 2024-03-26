9998f22c15616ce5a60dd072b1db9a0c089bd318
serjo96 <promlemboy@gmail.com>
2024-03-27 01:23:48 +0700

    Added  new header menu

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,34 +1,70 @@
 import React from 'react';
-import {AppBar, Switch, Toolbar, Typography, Button, Box} from '@mui/material';
+import {AppBar, Switch, Toolbar, Typography, Button, Box, Menu, MenuItem} from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 import {useThemeContext} from "~/services/themes/ThemeContext";
-import {Link} from "react-router-dom";
+import {Link as RouterLink, Link} from "react-router-dom";
 import {useAuth} from "~/features/auth/cotext/useAuth";
 
 
-const HeaderLinks = ({ isAuthenticated }: {isAuthenticated: boolean}) => (
-  isAuthenticated ? (
-    <Link to={'/profile'}>
-      <Button sx={{ color: 'white' }} variant="outlined" color="inherit">
-        <AccountCircleIcon />
-      </Button>
-    </Link>
-  ) : (
-    <Link to={'/sign-in'}>
-      <Button sx={{ color: 'white' }} variant="outlined" color="inherit">
-        Login
-      </Button>
-    </Link>
-  )
-);
+
 
 const Header = () => {
   const { toggleTheme, mode } = useThemeContext();
-  const {isAuthenticated} = useAuth()
+  const {isAuthenticated, logout} = useAuth()
+
+
+  const UserMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
+    if (!isAuthenticated) {
+      return (
+        <Link to={'/sign-in'}>
+          <Button sx={{color: 'white'}} variant="outlined" color="inherit">
+            Login
+          </Button>
+        </Link>
+      );
+    }
+
+    return (
+      <React.Fragment>
+        <Button
+          id="basic-button"
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          <AccountCircleIcon/>
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem>
+            <RouterLink to={'/profile'}>My account</RouterLink>
+          </MenuItem>
+          <MenuItem onClick={logout}>Logout</MenuItem>
+        </Menu>
+      </React.Fragment>
+    );
+  };
 
   return (
     <AppBar position="static">
@@ -45,7 +81,7 @@ const Header = () => {
             icon={< Brightness7Icon/>}
             checkedIcon={<Brightness4Icon />}
           />
-          <HeaderLinks isAuthenticated={isAuthenticated}></HeaderLinks>
+          <UserMenu />
         </Box>
       </Toolbar>
     </AppBar>
