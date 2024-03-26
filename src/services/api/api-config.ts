5ad337e338bc1bd9cfcf7a0daa8ec239b1ad1551
serjo96 <promlemboy@gmail.com>
2024-03-27 01:23:07 +0700

    Added access token fo config api

--- src/services/api/api-config.ts
+++ src/services/api/api-config.ts
@@ -1,9 +1,11 @@
 import {Configuration} from "~/services/api/open-api";
 import {refreshTokenMiddleware} from "~/services/api/middleware/refresh-token.middleware";
 import {errorHandlingMiddleware} from "~/services/api/middleware/error-handling.middleware";
-import config from "~/core/config/config";
+import {Tokens} from "~/core/constants";
+import config from "~/core/config";
 
 export default new Configuration({
   basePath: config.apiUrl,
+  accessToken: localStorage.getItem(Tokens.ACCESS_TOKEN) || '',
   middleware: [refreshTokenMiddleware, errorHandlingMiddleware]
 })
